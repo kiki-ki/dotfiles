eb5709252b09502f92d3ff10c59183f9382cbb18
kiki-ki <malpie0619@gmail.com>
2023-03-19 12:40:24 +0900

    add vscode setting

---
+++ scripts/init_vscode.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+init_settings () {
+  local source_dir="$HOME"/vscode_settings
+  local deploy_dir="$HOME"/Library/Application\ Support/Code/User/
+
+  cat "$source_dir"/settings.json > "$deploy_dir"/settings.json
+  cat "$source_dir"/keybindings.json > "$deploy_dir"/keybindings.json
+
+  while read -r line; do code --install-extension "$line"; done < "$source_dir"/extensions.txt
+}
+
+echo "VSCode settings overwrite current VSCode settings, OK? [Yy/n]: "
+read -r yn
+
+if [ ! "$yn" = "Y" ] && [ ! "$yn" = "y" ]; then
+  echo "canceled."
+  exit 1
+fi
+
+init_settings
+echo "init vscode settings completed."
+
